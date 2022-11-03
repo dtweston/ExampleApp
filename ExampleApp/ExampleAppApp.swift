01a46b709b122cb27973abc719df80bcfee5c56d
Dave Weston <dtweston@gmail.com>
2022-11-03 15:19:01 -0700

    Initial Commit

---
+++ ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Dave Weston on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
